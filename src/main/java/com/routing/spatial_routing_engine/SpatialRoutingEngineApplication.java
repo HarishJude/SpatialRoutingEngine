b2558ffaddf17bec8a1d2203b2358e8f77160be1
HarishJude <harishjudeofficial@gmail.com>
2026-09-01 17:09:50 +0530

    initial commit : added springboot dependencies

---
+++ src/main/java/com/routing/spatial_routing_engine/SpatialRoutingEngineApplication.java
@@ -0,0 +1,13 @@
+package com.routing.spatial_routing_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpatialRoutingEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpatialRoutingEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/routing/spatial_routing_engine/SpatialRoutingEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.routing.spatial_routing_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpatialRoutingEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
